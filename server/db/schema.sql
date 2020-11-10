b3c796a51f44132f5921048a7f24279a21ce68ed
germanmorenoortiz <germanmorenoortiz.com>
2020-11-10 20:50:03 +0100

    base del esquema

---
+++ server/db/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE recetas (
+  receta_id serial PRIMARY KEY  
+, name_receta    text NOT NULL
+);
+
+CREATE TABLE ingredientes (
+  ingrediente_id  serial PRIMARY KEY
+, name_ingrediente     text NOT NULL
+);
+
+CREATE TABLE receta_ingrediente (
+  receta_id int NOT NULL,
+  ingrediente_id int NOT NULL,
+  PRIMARY KEY (receta_id, ingrediente_id),
+  FOREIGN KEY (ingrediente_id) REFERENCES ingredientes(ingrediente_id) ON UPDATE CASCADE,
+  FOREIGN KEY (receta_id) REFERENCES recetas(receta_id) ON UPDATE CASCADE
+);
