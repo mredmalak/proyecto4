4718c85814e322ef4dc6d7794645818fa7fde65f
eduardo <eduvela_94@hotmail.com>
2020-11-11 09:39:28 +0100

    Server

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,11 +1,13 @@
 CREATE TABLE recetas (
   receta_id serial PRIMARY KEY  
 , name_receta    text NOT NULL
+, procces_receta text NOT NULL
 );
 
 CREATE TABLE ingredientes (
   ingrediente_id  serial PRIMARY KEY
 , name_ingrediente     text NOT NULL
+, amount_ingrediente integer NOT NULL
 );
 
 CREATE TABLE receta_ingrediente (
