0d370eb19249a8f4436678612c14286b8c91fb14
germanmorenoortiz <germanmorenoortiz.com>
2020-11-12 17:03:10 +0100

    plis work

--- server/db/queries/recetario.js
+++ server/db/queries/recetario.js
@@ -3,6 +3,15 @@ import { query } from '../index.js';
 const getAll = async () => {
   const recetarios = await query({
     tag: 'recetarios.getAll',
+    queryString: `SELECT name_receta FROM recetas`,
+  });
+  return recetarios;
+};
+
+
+const getIngredientesDeReceta = async (receta) => {
+  const recetarios = await query({
+    tag: 'recetarios.getIngreidentesDeReceta',
     queryString: `SELECT
     recetas.name_receta,
     ingredientes.name_ingrediente,
@@ -10,9 +19,12 @@ const getAll = async () => {
 FROM
     receta_ingrediente
     INNER JOIN recetas ON receta_ingrediente.receta_id = recetas.receta_id
-    INNER JOIN ingredientes ON receta_ingrediente.ingrediente_id = ingredientes.ingrediente_id;`,
+    INNER JOIN ingredientes ON receta_ingrediente.ingrediente_id = ingredientes.ingrediente_id
+WHERE
+    recetas.name_receta = '${name}'`,
   });
   return recetarios;
 };
 
-export default { getAll };
+
+export default { getAll, getIngredientesDeReceta };
--- server/models/recetario.js
+++ server/models/recetario.js
@@ -1,10 +1,28 @@
 import Recetario from '../db/queries/recetario.js';
-
-export const getListRecetario = async () => {
+const haveQuery = (object) => {
+  return Object.entries(object).length > 0;
+};
+export const getListRecetario = async (query) => {
+  console.log(haveQuery(query));
   try {
+    if (haveQuery(query)) {
+      console.log('alex es el amo')
+      const name = query['receta'];
+      const ingredientesDeReceta = Recetario.getIngredientesDeReceta(name);
+      return ingredientesDeReceta;
+    }
     const recetarios = Recetario.getAll();
     return recetarios;
   } catch (error) {
     throw new Error(error);
   }
 };
+
+// export const getListRecetario = async () => {
+//   try {
+//     const recetarios = Recetario.getAll();
+//     return recetarios;
+//   } catch (error) {
+//     throw new Error(error);
+//   }
+// };
