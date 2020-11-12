e79c55052312c885da12a94b03f8068f876af1bd
germanmorenoortiz <germanmorenoortiz.com>
2020-11-12 14:51:04 +0100

    change schema and seed

--- server/db/queries/recetario.js
+++ server/db/queries/recetario.js
@@ -3,7 +3,14 @@ import { query } from '../index.js';
 const getAll = async () => {
   const recetarios = await query({
     tag: 'recetarios.getAll',
-    queryString: `SELECT name_receta FROM recetas`,
+    queryString: `SELECT
+    recetas.name_receta,
+    ingredientes.name_ingrediente,
+    amount_ingrediente
+FROM
+    receta_ingrediente
+    INNER JOIN recetas ON receta_ingrediente.receta_id = recetas.receta_id
+    INNER JOIN ingredientes ON receta_ingrediente.ingrediente_id = ingredientes.ingrediente_id;`,
   });
   return recetarios;
 };
