7848de2789d8bea6ff03aaa6a9a73298299f98d7
germanmorenoortiz <germanmorenoortiz.com>
2020-11-12 09:07:32 +0100

    semillas de la db recetario

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,24 +1,16 @@
-DROP TABLE recetas IF EXISTS;
-DROP TABLE ingredientes IF EXISTS;
+
+DROP TABLE IF EXISTS  receta_ingrediente ;
+DROP TABLE IF EXISTS  recetas ;
+DROP TABLE IF EXISTS  ingredientes ;
 
 
-CREATE TABLE recetas (
-  receta_id serial PRIMARY KEY,
-  name_receta    text NOT NULL,
-  procces_receta VARCHAR (255),
-  porciones_receta INT,
-);
+CREATE TABLE recetas (receta_id serial PRIMARY KEY, name_receta text NOT NULL, procces_receta VARCHAR (255), porciones_receta INT NOT NULL);
 
-CREATE TABLE ingredientes (
-  ingrediente_id  serial PRIMARY KEY, 
-  name_ingrediente     text NOT NULL,
-  amount_ingrediente VARCHAR (255) NOT NULL
-);
+CREATE TABLE ingredientes (ingrediente_id serial PRIMARY KEY, name_ingrediente text NOT NULL, amount_ingrediente VARCHAR (255) NOT NULL);
 
 CREATE TABLE receta_ingrediente (
   receta_id int NOT NULL,
   ingrediente_id int NOT NULL,
   PRIMARY KEY (receta_id, ingrediente_id),
   FOREIGN KEY (ingrediente_id) REFERENCES ingredientes(ingrediente_id) ON UPDATE CASCADE,
-  FOREIGN KEY (receta_id) REFERENCES recetas(receta_id) ON UPDATE CASCADE
-);
+  FOREIGN KEY (receta_id) REFERENCES recetas(receta_id) ON UPDATE CASCADE);
