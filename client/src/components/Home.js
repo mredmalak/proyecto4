c8028c067e05204cb8f43992bdb22a6ebf864d30
germanmorenoortiz <germanmorenoortiz.com>
2020-11-16 13:45:47 +0100

    newIngrediente

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,13 +7,23 @@ const Home = () => {
       <h1>Bienvenidos</h1>
       <h2>¿Que Quieres Comer Hoy?</h2>
       <p>Escoge Tus Ingredientes</p>
-      <Link to="/ingredientes"><button>Ingredientes</button></Link>
+      <Link to="/ingredientes">
+        <button>Ingredientes</button>
+      </Link>
       <p>O si quieres ver las recetas del recetario...</p>
-      <Link to="/recetas"><button>Recetas</button></Link>
+      <Link to="/recetas">
+        <button>Recetas</button>
+      </Link>
       <p>O quieres crear una receta nueva...</p>
-      <Link to="/recetas/new"><button>Nueva receta</button></Link>
+      <Link to="/recetas/new">
+        <button>Nueva receta</button>
+      </Link>
+      <p>O quieres crear un ingrediente nuevo...</p>
+      <Link to="/ingredientes/new">
+        <button>Nuevo ingrediente</button>
+      </Link>
     </>
-  )
+  );
 }
 
 export default Home
