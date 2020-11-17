396c650884ff87e55e116cb42d0762b99d2679c5
eduardo <eduvela_94@hotmail.com>
2020-11-17 10:23:38 +0100

    enable_access_alldevelopers

--- server/db/index.js
+++ server/db/index.js
@@ -5,7 +5,7 @@ const { Pool } = pg;
 // ! Cambiar los datos de la linea 8 para que este correcto en tu local
 
 const pool = new Pool({
-  connectionString: 'postgresql://oscar_german:valerialigiasami@localhost:5432/recetario',
+  connectionString: 'postgresql://eduardo:1@localhost:5432/recetario',
 });
 export const query = async ({ tag = '', queryString, params }) => {
   try {
