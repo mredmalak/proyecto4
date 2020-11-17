01fe6bad69c22bf771d47011fae4371c12aa000e
eduardo <eduvela_94@hotmail.com>
2020-11-17 15:52:13 +0100

    heroku deploy

--- server/db/index.js
+++ server/db/index.js
@@ -6,7 +6,8 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString:
-    'postgresql://oscar_german:valerialigiasami@localhost:5432/recetario',
+    process.env.DATABSE_URL ||
+    'postgresql://eduardo:1@localhost:5432/recetario',
 });
 export const query = async ({ tag = '', queryString, params }) => {
   try {
